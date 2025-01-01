6432f46ade2239864738a4d964fd1f87b6e5f2e3
Tatiana Johnson <tatianarjo@gmail.com>
2024-12-31 20:55:41 -0600

    displays cart only when item is added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ export function Navbar() {
                         About
                     </Nav.Link>
                 </Nav>
-                <Button
+                {cartQuantity > 0 && ( <Button
                 onClick={openCart}
                 style={{ width: "3rem", height: "3rem", position: "relative" }}
                     variant="outline-primary" className="rounded-circle"
@@ -41,8 +41,9 @@ export function Navbar() {
                     >
                         {cartQuantity}
                     </div>
-                </Button>
+                </Button>)}
             </Container>
         </NavbarBs>
     )
 }
+
